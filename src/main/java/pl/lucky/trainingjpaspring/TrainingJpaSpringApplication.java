b94e96d8cf24550aae9a5b6b869ce639d72ce43a
lucki90 <tomlucki@wp.pl>
2019-06-16 18:28:22 +0200

    init

---
+++ src/main/java/pl/lucky/trainingjpaspring/TrainingJpaSpringApplication.java
@@ -0,0 +1,13 @@
+package pl.lucky.trainingjpaspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingJpaSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrainingJpaSpringApplication.class, args);
+    }
+
+}
