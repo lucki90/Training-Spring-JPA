32352f3780d59d6ff030773c724d1723ba59ec31
lucki90 <tomlucki@wp.pl>
2019-06-19 20:52:38 +0200

    added @transactional

--- src/main/java/pl/lucky/trainingjpaspring/dao/BookDaoImpl.java
+++ src/main/java/pl/lucky/trainingjpaspring/dao/BookDaoImpl.java
@@ -1,36 +1,28 @@
 package pl.lucky.trainingjpaspring.dao;
 
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import pl.lucky.trainingjpaspring.model.Book;
 
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.PersistenceUnit;
+import javax.persistence.PersistenceContext;
 
 @Repository
 public class BookDaoImpl implements BookDao {
 
-    @PersistenceUnit
-    private EntityManagerFactory emf;
+    @PersistenceContext
+    private EntityManager em;
 
     public BookDaoImpl() {
     }
 
+    @Transactional
     public void save(Book book) {
-        EntityManager em = emf.createEntityManager();
-        EntityTransaction tx = em.getTransaction();
-        tx.begin();
-
         em.persist(book);
-        tx.commit();
-        em.close();
     }
 
     public Book get(Long id) {
-        EntityManager em = emf.createEntityManager();
         Book book = em.find(Book.class, id);
-        em.close();
         return book;
     }
 }
